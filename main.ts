45c583c26cc4a170be6a473d85b4e36fedd8c7ad
JJNMSJ <83886060+JJNMSJ@users.noreply.github.com>
2021-05-10 17:07:43 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,131 @@
+basic.forever(function () {
+    music.playMelody("C5 B A G F E D C ", 120)
+    basic.showIcon(IconNames.Heart)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . # # # .
+        . . # . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . # . . .
+        . # . . .
+        # # . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(3000)
+    basic.showLeds(`
+        # # . . .
+        # . . . .
+        # . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(4000)
+    basic.showLeds(`
+        # . . . .
+        . . . . .
+        # # . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(4000)
+    basic.showLeds(`
+        # # . . .
+        . . . . .
+        # . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(3000)
+    basic.showLeds(`
+        # . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . . . . .
+        # . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        # . . . .
+        # . . . .
+        # . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(3000)
+    basic.showLeds(`
+        # . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        # . . . .
+        # . . . .
+        # # . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(3000)
+    basic.showLeds(`
+        # . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        # # . . .
+        . # . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(3000)
+    basic.showLeds(`
+        # . . . .
+        . # . . .
+        # . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(4000)
+    basic.showLeds(`
+        # . . . .
+        # # . . .
+        # . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(3000)
+    basic.showLeds(`
+        # . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . # . . .
+        . # . . .
+        # # . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(3000)
+})
